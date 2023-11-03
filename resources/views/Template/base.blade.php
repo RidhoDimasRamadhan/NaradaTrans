b3b7687a5c96fcbd68003499ebf475710e165f7d
alpinn <alfinasyah36@gmail.com>
2023-11-03 21:53:13 +0700

    create a footer component

--- resources/views/Template/base.blade.php
+++ resources/views/Template/base.blade.php
@@ -13,14 +13,17 @@
   </head>
   <body>
     
-    <div class="container">
+    <div class="">
       {{-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
           <a class="navbar-brand" href="#">Navbar</a>
       </nav> --}}
       @yield('content')
       {{-- <h1>Ridho</h1> --}}
 
-      </div>
+    </div>
+    <div>
+      
+    </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </html>
--- resources/views/main_page.blade.php
+++ resources/views/main_page.blade.php
@@ -4,48 +4,121 @@
 @section('content')
 {{-- <div class="container"> --}}
 
-    <div class="navbar">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-          
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
-                  </a>
-                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                  </div>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link disabled" href="#">Disabled</a>
-                </li>
-              </ul>
+
+<div class="container" style="height: 50vh;">
+
+</div>
+
+<!-- Footer -->
+<footer class="text-center text-lg-start bg-footer text-muted">
+
+  <!-- Section: Links  -->
+  <section class="pt-5">
+    <div class="container-fluid text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <img src="{{ URL('images/Logo.png') }}" alt="Logo" class="logo">
+          <div class="address">
+            <h6 class="fw-bold text-white mt-4">Pool Bus dan Kantor Pusat</h6>
+            <p class="text-white">
+              Jl. Jatimulya Raya No.1, Jatimulya, Kec. Tambun Selatan, Kota Bekasi, Jawa Barat 17510
+            </p>
+          </div>
+
+          <div class="contact mb-4">
+            
+            <div class="d-inline-block">
+              <p class="text-white">
+                <img src="{{ URL('images/phoneIco.png') }}" alt="" class="bg-light ico">
+                085311301830
+              </p>
             </div>
-          </nav>
+
+            <div class="d-inline-block mail">
+              <p class="text-white">
+                <img src="{{ URL('images/webIco.png') }}" alt="" class="bg-light ico">
+                naradatrans@gmail.com
+              </p>
+            </div>
+
+            <div class="">
+              <p class="text-white">
+                <img src="{{ URL('images/mailIco.png') }}" alt="" class="bg-light ico">
+                naradatrans.co.id
+              </p>
+            </div>
+          </div>
+
+          <div class="d-flex gap-4">
+            <div class="">
+              <img src="{{ URL('images/twtIco.png') }}" alt="" class="bg-light socialIco">
+            </div>
+            <div>
+              <img src="{{ URL('images/fbIco.png') }}" alt="" class="bg-light socialIco">
+            </div>
+            <div>
+              <img src="{{ URL('images/igIco.png') }}" alt="" class="bg-light socialIco">        
+            </div>
+          </div>
+
+          <div class="mt-4 text-light copyright">
+            <p>© 2023 Team NaradaTrans, All Rights Reserved</p>
+          </div>
+
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-white mb-4">
+            Menu Utama
+          </h6>
+          <div class="d-flex justify-content-center justify-content-lg-between border-bottom mb-3 line">
+            <a href="#" class="text-white pb-3">Home</a>
+          </div>
+          <div class="d-flex justify-content-center justify-content-lg-between border-bottom mb-3 line">
+            <a href="#" class="text-white pb-3">Profil</a>
+          </div>
+          <div class="d-flex justify-content-center justify-content-lg-between border-bottom mb-3 line">
+            <a href="#" class="text-white pb-3">Kontak</a>
+          </div>
+          
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-white mb-4">
+            Jam Operasional
+          </h6>
+          <div class="d-flex justify-content-center justify-content-lg-between border-bottom mb-3">
+            <span class="text-white pb-3">Senin - Jumat :</span>
+            <span class="text-white">09.00 pm - 13.00 pm</span>
+          </div>
+          <div class="d-flex justify-content-center justify-content-lg-between border-bottom mb-3">
+            <span class="text-white pb-3">Sabtu :</span>
+            <span class="text-white">09.00 pm - 13.00 pm</span>
+          </div>
+          <div class="d-flex justify-content-center justify-content-lg-between border-bottom mb-3">
+            <span class="text-white pb-3">Minggu :</span>
+            <div class="libur">
+              <span class="text-white">Libur</span>
+            </div>
+          </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
     </div>
+  </section>
+  <!-- Section: Links  -->
 
-
-<main>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos ut laborum facere natus ullam voluptatibus pariatur deleniti cupiditate, nihil tenetur, quo eum in saepe officiis non laboriosam blanditiis aspernatur assumenda omnis culpa. Odit quos voluptates quasi numquam dolore cupiditate aliquam! Enim ipsam rem harum optio ex reiciendis distinctio eius excepturi, porro tenetur quasi! Numquam inventore nesciunt sapiente pariatur culpa. Consequatur voluptatem est, voluptate corrupti, quis illum optio mollitia nulla dolorem commodi alias quidem facilis molestiae nostrum assumenda! Facilis corporis explicabo, ab maiores vitae quasi cupiditate incidunt modi perspiciatis ipsa beatae facere debitis eius velit. Animi repellat, hic adipisci molestiae ad doloribus voluptate quisquam fuga. Quia, voluptate atque assumenda, cum totam commodi veniam optio unde delectus officia, ex dolorem labore nobis! Repellendus tempore repudiandae quos molestias eius, libero ab ducimus sed quo ullam, eum repellat soluta? Tempore deleniti quae sed saepe magnam officiis! Culpa labore nisi assumenda harum rerum consequuntur cumque ipsa quos quisquam quam, porro, ducimus corporis necessitatibus neque et accusamus quo, iusto voluptatibus quod odio asperiores illo est quaerat quae. Libero temporibus modi obcaecati debitis! Architecto dolorum assumenda reprehenderit ab veritatis facere culpa, adipisci, ducimus at laudantium repellendus nesciunt ex porro, dolores id incidunt minima corrupti. Quo, itaque omnis?</p>
-</main>
-
-    <footer>
-        <p>footer disini ya pin</p>
-    </footer>
-
+  
+</footer>
+<!-- Footer -->
 {{-- </div> --}}
 @endsection
