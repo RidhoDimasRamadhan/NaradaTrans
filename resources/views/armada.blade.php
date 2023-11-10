d3d0fb90369194486a29bf18feabe833a40e085b
RidhoDimasRamadhan <ridhoodimass22@gmail.com>
2023-11-10 09:27:17 +0700

    nambahin sedikit doang

--- resources/views/armada.blade.php
+++ resources/views/armada.blade.php
@@ -3,7 +3,7 @@
 <script src="https://kit.fontawesome.com/08692cf6f2.js" crossorigin="anonymous"></script>
 
 @section('content')
-<div class="container" style="margin-top:7rem;">
+<div class="container" style="margin-top:10rem;">
 
 <h1>Halaman armada</h1>
 </div>
--- resources/views/harga.blade.php
+++ resources/views/harga.blade.php
@@ -3,9 +3,7 @@
 <script src="https://kit.fontawesome.com/08692cf6f2.js" crossorigin="anonymous"></script>
 
 @section('content')
-<div class="container" style="margin-top:7rem;">
-
-<h1>Halaman Harga</h1>
-</div>
+<div class="container" style="margin-top:10rem;position:absolute;border:1px solid black;">
+    <h1>Halaman Harga</h1>  
 </div>
 @endsection
