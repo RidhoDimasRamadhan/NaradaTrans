2591b74ef2fec041cc6e62f41fc3287afe7dd57e
RidhoDimasRamadhan <ridhoodimass22@gmail.com>
2023-11-10 17:08:37 +0700

    edit main_page 70%

--- resources/views/main_page.blade.php
+++ resources/views/main_page.blade.php
@@ -56,11 +56,14 @@
 
 <section class="d-flex justify-content-center justify-content-lg-between section">
   <div class="container-fluid text-md-start">
-    <h1 class="w-50 mx-auto text-center profile-title fw-bold text-black">Mengapa Harus menggunakan <span class="naradaText">NaradaTrans ?</span></h1>
+    <div class="mengapa">
+      <h1 class="mx-auto text-center profile-title fw-bold text-black">Mengapa Harus menggunakan <span class="naradaText">NaradaTrans ?</span></h1>
+
+    </div>
     <div class="container">
       <div class="row mt-3">
         <div class="col-md-3 col-lg-4 col-xl-3">
-          <div class="mx-5">
+          <div class="mx-5 img">
             <img src="{{ URL('images/armadaBadge.png') }}" alt="">
           </div>
           <div class="mt-3 text-center">
@@ -69,7 +72,7 @@
           </div>
       </div>
         <div class="col-md-3 col-lg-4 col-xl-3">
-          <div class="mx-5">
+          <div class="mx-5 img">
             <img src="{{ URL('images/ruteBadge.png') }}" alt="">
           </div>
           <div class="mt-3 text-center">
@@ -78,7 +81,7 @@
           </div>
       </div>
         <div class="col-md-3 col-lg-4 col-xl-3">
-          <div class="mx-5">
+          <div class="mx-5 img">
             <img src="{{ URL('images/layananBadge.png') }}" alt="">
           </div>
           <div class="mt-3 text-center">
@@ -87,7 +90,7 @@
           </div>
       </div>
         <div class="col-md-3 col-lg-4 col-xl-3">
-          <div class="mx-5">
+          <div class="mx-5 img">
             <img src="{{ URL('images/hargaBadge.png') }}" alt="">
           </div>
           <div class="mt-3 text-center">
@@ -98,11 +101,21 @@
       </div>
     </div>
     <h1 class="w-50 mx-auto text-center profile-title fw-bold text-black">Armada<span class="naradaText"> Kami</span></h1>
-    <!-- <div class="container-fluid">
-      <img src="{{ URL('images/busPutih.png') }}" alt="">
-      <img src="{{ URL('images/busYellow.jpg') }}" alt="" class="busKuning">
-      <img src="{{ URL('images/busBiru.png') }}" alt="">
-    </div> -->
+    <div class="container-fluid">
+    <div class="wrapper">
+      <i id="left" class="fa-solid fa-angle-left"></i>
+      <div class="carousel">
+        <img src="{{ URL('images/busPutih.png') }}" alt="img" draggable="false">
+        <img src="{{ URL('images/busYellow.jpg') }}" alt="img" draggable="false">
+        <img src="{{ URL('images/busBiru.png') }}" alt="img" draggable="false">
+      </div>
+      <i id="right" class="fa-solid fa-angle-right"></i>
+    </div>
+
+      <!-- <img src="" alt="">
+      <img src="" alt="" class="busKuning">
+      <img src="" alt=""> -->
+    </div>
   </div>
 </section>
 
@@ -255,6 +268,81 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+
+
+const carousel = document.querySelector(".carousel"),
+firstImg = carousel.querySelectorAll("img")[0],
+arrowIcons = document.querySelectorAll(".wrapper i");
+
+let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
+
+const showHideIcons = () => {
+    // showing and hiding prev/next icon according to carousel scroll left value
+    let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // getting max scrollable width
+    arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+    arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
+}
+
+arrowIcons.forEach(icon => {
+    icon.addEventListener("click", () => {
+        let firstImgWidth = firstImg.clientWidth + 14; // getting first img width & adding 14 margin value
+        // if clicked icon is left, reduce width value from the carousel scroll left else add to it
+        carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+        setTimeout(() => showHideIcons(), 60); // calling showHideIcons after 60ms
+    });
+});
+
+const autoSlide = () => {
+    // if there is no image left to scroll then return from here
+    if(carousel.scrollLeft - (carousel.scrollWidth - carousel.clientWidth) > -1 || carousel.scrollLeft <= 0) return;
+
+    positionDiff = Math.abs(positionDiff); // making positionDiff value to positive
+    let firstImgWidth = firstImg.clientWidth + 14;
+    // getting difference value that needs to add or reduce from carousel left to take middle img center
+    let valDifference = firstImgWidth - positionDiff;
+
+    if(carousel.scrollLeft > prevScrollLeft) { // if user is scrolling to the right
+        return carousel.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+    }
+    // if user is scrolling to the left
+    carousel.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+}
+
+const dragStart = (e) => {
+    // updatating global variables value on mouse down event
+    isDragStart = true;
+    prevPageX = e.pageX || e.touches[0].pageX;
+    prevScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+    // scrolling images/carousel to left according to mouse pointer
+    if(!isDragStart) return;
+    e.preventDefault();
+    isDragging = true;
+    carousel.classList.add("dragging");
+    positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff;
+    showHideIcons();
+}
+
+const dragStop = () => {
+    isDragStart = false;
+    carousel.classList.remove("dragging");
+
+    if(!isDragging) return;
+    isDragging = false;
+    autoSlide();
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("touchstart", dragStart);
+
+document.addEventListener("mousemove", dragging);
+carousel.addEventListener("touchmove", dragging);
+
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("touchend", dragStop);
 </script>
 <!-- Footer -->
 {{-- </div> --}}
