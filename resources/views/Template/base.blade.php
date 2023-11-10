b908c3b6b2c49a9dfbf71f5149e5ce03875ecfa6
alpinn <alfinasyah36@gmail.com>
2023-11-10 22:54:28 +0700

    fixed responsive for 576px, 768px, 992px, 1200px at armada section and footer section

--- resources/views/Template/base.blade.php
+++ resources/views/Template/base.blade.php
@@ -29,7 +29,7 @@
           </button>
 
           <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="col-12">
+            <div class="col-lg-12 col-sm-1">
             <div class="row">
               <div class="col-4"></div>
               <div class="col-8">
@@ -45,7 +45,7 @@
                     <div class="d-flex gap-4 mt-2 ms-3">
                       <div>
                         <a href="#">
-                          <img src="{{ URL('images/twtNavbar.png') }}" alt="twitter" class="">
+                          <img src="{{ URL('images/twtNavbar.png') }}" alt="twitter" class="navbarIco">
                         </a>
                       </div>
                       <div>
@@ -55,7 +55,7 @@
                       </div>
                       <div>
                         <a href="#">
-                          <img src="{{ URL('images/igNavbar.png') }}" alt="Instagram" class="">        
+                          <img src="{{ URL('images/igNavbar.png') }}" alt="Instagram" class="navbarIco">        
                         </a>
                       </div>
                     </div>
--- resources/views/main_page.blade.php
+++ resources/views/main_page.blade.php
@@ -5,57 +5,8 @@
 @section('content')
 {{-- <div class="container"> --}}
 
-<nav class="navbar navbar-expand-lg navbar-light" id="navbar">
-  <div class="container-fluid">
-    <a class="navbar-brand mt-2 mt-lg-0" href="#">
-        <img src="{{ URL('images/Logo1.png') }}" alt="Logo">
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <div class="d-flex d-md-block my-md-1 align-items-center navbarGap">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item mx-3">
-              <a class="navbarLink" id="active" href="#home">Beranda</a>
-            </li>
-            <li class="nav-item mx-3">
-              <a class="navbarLink" href="#">Profil</a>
-            </li>
-            <li class="nav-item mx-3">
-              <a class="navbarLink" href="#">Armada</a>
-            </li>
-            <li class="nav-item mx-3">
-              <a class="navbarLink" href="#">Galeri</a>
-            </li>
-            <li class="nav-item mx-3">
-              <a class="navbarLink" href="#">Harga</a>
-            </li>
-            <li class="nav-item mx-3">
-              <a class="navbarLink" href="#">Kontak</a>
-            </li>
-          </ul>
-          <div class="d-flex gap-4">
-            <div>
-              <a href="#">
-                <img src="{{ URL('images/twtNavbar.png') }}" alt="twitter" class="navbarIco">
-              </a>
-            </div>
-            <div>
-              <a href="#">
-                <img src="{{ URL('images/fbNavbar.png') }}" alt="Facebook" class="">
-              </a>
-            </div>
-            <div>
-              <a href="#">
-                <img src="{{ URL('images/igNavbar.png') }}" alt="Instagram" class="navbarIco">        
-              </a>
-            </div>
-          </div>
-        </div>
-    </div>
-  </div>
-</nav>
+<div id="navbar">
+</div>
 
 <!-- Crousserl -->
 
@@ -105,16 +56,17 @@
 
 <section class="d-flex justify-content-center justify-content-lg-between section">
   <div class="container-fluid text-md-start">
-    <h1 class="w-50 mx-auto text-center profile-title fw-bold text-black">Mengapa Harus menggunakan <span class="naradaText">NaradaTrans ?</span></h1>
-    <div class="container">
-      <div class="row mt-3">
-        <div class="col-md-3 col-lg-4 col-xl-3">
+    <div class="bg-Grey py-2">
+      <h1 class="text-sm-start text-md-center text-lg-center text-xl-center profile-title fw-bold text-black">Mengapa Harus menggunakan <span class="naradaText">NaradaTrans ?</span></h1>
+      <div class="container-fluid">
+        <div class="row mt-3">
+        <div class="col-md-3 col-lg-7 col-xl-3">
           <div class="mx-5 img">
             <img src="{{ URL('images/armadaBadge.png') }}" alt="">
           </div>
           <div class="mt-3 text-center">
             <h4 class="title-card">Armada Modern</h4>
-            <p class="descProfile px-3">Armada modern dan nyaman dengan fasilitas AC, WiFi, TV, dan keamanan terkini.</p>
+            <p class="descProfile px-2 px-lg-5">Armada modern dan nyaman dengan fasilitas AC, WiFi, TV, dan keamanan terkini.</p>
           </div>
       </div>
         <div class="col-md-3 col-lg-4 col-xl-3">
@@ -125,8 +77,8 @@
             <h4 class="title-card">Rute Luas</h4>
             <p class="descProfile px-3">Rute luas, fleksibel, dan sesuai berbagai kebutuhan pelanggan.</p>
           </div>
-      </div>
-        <div class="col-md-3 col-lg-4 col-xl-3">
+        </div>
+        <div class="col-md-3 col-lg-7 col-xl-3">
           <div class="mx-5 img">
             <img src="{{ URL('images/layananBadge.png') }}" alt="">
           </div>
@@ -134,7 +86,7 @@
             <h4 class="title-card">Layanan Profesional</h4>
             <p class="descProfile px-3">Layanan pelanggan ramah, responsif, dan berpengetahuan untuk pengalaman pelanggan positif.</p>
           </div>
-      </div>
+        </div>
         <div class="col-md-3 col-lg-4 col-xl-3">
           <div class="mx-5 img">
             <img src="{{ URL('images/hargaBadge.png') }}" alt="">
@@ -146,49 +98,39 @@
         </div>
       </div>
     </div>
-    <h1 class="w-50 mx-auto text-center profile-title fw-bold text-black">Armada<span class="naradaText"> Kami</span></h1>
-    <!-- <div class="container-fluid">
-      <img src="{{ URL('images/busPutih.png') }}" alt="">
-      <img src="{{ URL('images/busYellow.jpg') }}" alt="" class="busKuning">
-      <img src="{{ URL('images/busBiru.png') }}" alt="">
+  </div>
+    <!-- <h1 class="w-50 mx-auto text-center fw-bold text-black">Armada<span class="naradaText"> Kami</span></h1>
+    <div class="container-fluid">
+      <div class="wrapper">
+        <i id="left" class="fa-solid fa-angle-left"></i>
+        <div class="carousel">
+          <img src="{{ URL('images/busPutih.png') }}" alt="img" draggable="false">
+        <img src="{{ URL('images/busYellow.jpg') }}" alt="img" draggable="false">
+        <img src="{{ URL('images/busBiru.png') }}" alt="img" draggable="false">
+      </div>
+      <i id="right" class="fa-solid fa-angle-right"></i>
+    </div>
     </div> -->
   </div>
-  
 </section>
+
 <!-- Profil Page -->
 
-<section class="d-flex justify-content-center justify-content-lg-between section">
-  <div class="container text-md-start">
-    <h1 class="w-75 mx-auto text-center profile-title fw-bold text-black">Armada <span class="naradaText">Kami</span></h1>
-  </div>
-</section>
-
-<!-- Testimoni -->
-<section class="d-flex justify-content-center justify-content-lg-between section">
-  <div class="container-fluid text-md-start bgGrey">
-    <h1 class="w-75 fw-bold text-black profile-title">Kesan Perjalanan Penumpang Bersama <span class="naradaText">NaradaTrans</span></h1>
-    
-  </div>
-  
-</section>
-
-<!-- Testimoni -->
-
 <!-- Footer -->
-<footer class="text-center text-lg-start bg-footer text-muted">
+<footer class="text-lg-start bg-footer text-muted">
 
   <!-- Section: Links  -->
   <section class="p-4 d-flex justify-content-center justify-content-lg-between">
     <div class="container-fluid text-md-start mt-5">
       <!-- Grid row -->
-      <div class="row mt-3">
+      <div class="row mt-lg-3 mt-sm-1">
         <!-- Grid column -->
-        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-lg-auto mb-4">
           <!-- Content -->
           <img src="{{ URL('images/Logo.png') }}" alt="Logo" class="logo">
           <div class="address">
             <h6 class="fw-bold text-white mt-4">Pool Bus dan Kantor Pusat</h6>
-            <p class="text-white">
+            <p class="text-white pe-sm-5">
               Jl. Jatimulya Raya No.1, Jatimulya, Kec. Tambun Selatan, Kota Bekasi, Jawa Barat 17510
             </p>
           </div>
@@ -203,14 +145,14 @@
 
             <div class="d-inline-block mail">
               <p class="text-white">
-                <img src="{{ URL('images/webIco.png') }}" alt="" class="bg-light ico">
+                <img src="{{ URL('images/mailIco.png') }}" alt="" class="bg-light ico">
                 naradatrans@gmail.com
               </p>
             </div>
 
             <div class="">
               <p class="text-white">
-                <img src="{{ URL('images/mailIco.png') }}" alt="" class="bg-light ico">
+                <img src="{{ URL('images/webIco.png') }}" alt="" class="bg-light ico">
                 naradatrans.co.id
               </p>
             </div>
@@ -236,18 +178,18 @@
         <!-- Grid column -->
 
         <!-- Grid column -->
-        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
           <!-- Links -->
           <h6 class="text-white mb-4">
             Menu Utama
           </h6>
-          <div class="d-flex justify-content-center justify-content-lg-between border-bottom mb-3 line">
+          <div class="d-flex justify-content-lg-between border-bottom mb-3 line">
             <a href="#" class="text-white pb-3">Home</a>
           </div>
-          <div class="d-flex justify-content-center justify-content-lg-between border-bottom mb-3 line">
+          <div class="d-flex justify-content-lg-between border-bottom mb-3 line">
             <a href="#" class="text-white pb-3">Profil</a>
           </div>
-          <div class="d-flex justify-content-center justify-content-lg-between border-bottom mb-3 line">
+          <div class="d-flex justify-content-lg-between border-bottom mb-3 line">
             <a href="#" class="text-white pb-3">Kontak</a>
           </div>
           
@@ -255,21 +197,21 @@
         <!-- Grid column -->
 
         <!-- Grid column -->
-        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+        <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
           <!-- Links -->
           <h6 class="text-white mb-4">
             Jam Operasional
           </h6>
-          <div class="d-flex justify-content-center justify-content-lg-between border-bottom mb-3">
+          <div class="d-flex justify-content-lg-between border-bottom mb-3">
             <span class="text-white pb-3">Senin - Jumat :</span>
             <span class="text-white">09.00 pm - 13.00 pm</span>
           </div>
-          <div class="d-flex justify-content-center justify-content-lg-between border-bottom mb-3">
-            <span class="text-white pb-3">Sabtu :</span>
+          <div class="d-flex justify-content-lg-between border-bottom mb-3">
+            <span class="text-white pb-3 me-md-2">Sabtu:</span>
             <span class="text-white">09.00 pm - 13.00 pm</span>
           </div>
-          <div class="d-flex justify-content-center justify-content-lg-between border-bottom mb-3">
-            <span class="text-white pb-3">Minggu :</span>
+          <div class="d-flex justify-content-lg-between border-bottom mb-3">
+            <span class="text-white pb-3 me-md-2">Minggu :</span>
             <div class="libur">
               <span class="text-white">Libur</span>
             </div>
