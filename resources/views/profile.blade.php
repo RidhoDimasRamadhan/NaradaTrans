ff6d11f24cfb70efbbe7d6ac24e2e3ebe8bde0e7
alpinn <alfinasyah36@gmail.com>
2023-11-23 16:46:15 +0700

    fixed galeri page

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -6,25 +6,21 @@
 <a href="https://wa.me/+6285281595005" class="up-icon"><i class="fa-brands fa-whatsapp" style="color: #00e034;"></i></a>
 <section class="d-flex justify-content-center justify-content-lg-between section" style="margin-top: 7rem;">
     <div class="container-fluid text-md-start bg-Grey">
-        <div class="d-lg-flex d-sm-block justify-content-lg-between">
-                <div class="article-left">
-                    <h3>Profil PO NaradaTrans</h3>
-                    <h2><span class="text-brown">Sejarah</span> NaradaTrans</h2>
-                    <p class="pt-3">NaradaTrans adalah perusahaan otobis yang khusus melayani pariwisata dan antar jemput karyawan dalam negeri. NaradaTrans didirikan pada tahun 2004 di Jakarta.</p>
-                    <p>Semua armada NaradaTrans menggunakan armada bis MERCEDES BENZ (Big Bus), Bus Medium (Mitsubishi) yang sudah terkenal akan standar kenyamanan dan keamanannya di bidang otomotif. Selain itu, kami juga menyediakan Mini Bus 15 seats menggunakan kendaraan seperti Isuzu Elf dan Toyota Commuter.</p>
-                    <p>Seiring dengan berjalannya waktu, NaradaTrans telah banyak melayani perusahaan-perusahaan swasta lokal dan asing, BUMN, sekolah-sekolah swasta, negeri maupun internasional, yayasan-yayasan sosial, dan acara-acara keluarga seperti perkawinan dan mudik.</p>
-                    <p>Komitmen NaradaTrans adalah memberikan layanan transportasi yang handal, nyaman, dan aman bagi setiap pelanggan kami. Dengan armada yang terjamin kualitasnya, kami siap memberikan pengalaman perjalanan yang memuaskan dan mengantarkan Anda dengan tepat waktu ke tujuan.</p>
-                </div> 
-            <div class="img-responsive text-center">
-                <img src="{{ URL('images/img1.png') }}" alt="img" class="img1">
-            </div>
-        </div>
+          <img src="{{ URL('images/img1.png') }}" alt="img" class="img-profile">
+          <div class="article-left">
+              <h3>Profil PO NaradaTrans</h3>
+              <h2><span class="text-brown">Sejarah</span> NaradaTrans</h2>
+              <p class="pt-3">NaradaTrans adalah perusahaan otobis yang khusus melayani pariwisata dan antar jemput karyawan dalam negeri. NaradaTrans didirikan pada tahun 2004 di Jakarta.</p>
+              <p>Semua armada NaradaTrans menggunakan armada bis MERCEDES BENZ (Big Bus), Bus Medium (Mitsubishi) yang sudah terkenal akan standar kenyamanan dan keamanannya di bidang otomotif. Selain itu, kami juga menyediakan Mini Bus 15 seats menggunakan kendaraan seperti Isuzu Elf dan Toyota Commuter.</p>
+              <p>Seiring dengan berjalannya waktu, NaradaTrans telah banyak melayani perusahaan-perusahaan swasta lokal dan asing, BUMN, sekolah-sekolah swasta, negeri maupun internasional, yayasan-yayasan sosial, dan acara-acara keluarga seperti perkawinan dan mudik.</p>
+              <p>Komitmen NaradaTrans adalah memberikan layanan transportasi yang handal, nyaman, dan aman bagi setiap pelanggan kami. Dengan armada yang terjamin kualitasnya, kami siap memberikan pengalaman perjalanan yang memuaskan dan mengantarkan Anda dengan tepat waktu ke tujuan.</p>
+          </div>
     </div>
 </section>
 
-<section class="d-flex justify-content-center justify-content-lg-between" style="margin-bottom: 2rem;">
+<section class="d-flex justify-content-center justify-content-lg-between">
     <div class="container-fluid text-md-start">
-        <div class="d-lg-flex d-sm-block justify-content-lg-between">
+        <div class="d-xl-flex justify-content-lg-between">
             <div class="article-img text-center">
                 <img src="{{ URL('images/img2.png') }}" alt="img" class="img2">
                 <img src="{{ URL('images/img3.png') }}" alt="img" class="img3">
